5f6d8d40b529ae56f0e3cfe64dcf6457b862b7d2
袁笛 <29752884+deasonyuan@users.noreply.github.com>
2021-04-19 19:48:27 +0800

    Update and rename Demo_easy.m to Demo.m

--- Demo.m
+++ Demo.m
@@ -1,10 +1,8 @@
-
 clear all;
 
 varargin=cell(1,2);
-
 varargin(1,1)={'train'};
-varargin(1,2)={struct('gpus', 3)};
+varargin(1,2)={struct('gpus', 1)};
 
 addpath(genpath('./base/matconvnet/matlab/'));
 run vl_setupnn ;
@@ -21,33 +19,12 @@ opts.imdbPath = fullfile(opts.expDir, 'imdb.mat') ;
 opts.imdbStatsPath = fullfile(opts.expDir, 'imdbStats.mat') ;
 opts.vocEdition = '11' ;
 opts.vocAdditionalSegmentations = false ;
-
 global resize;
-% display=1;
-%   g=gpuDevice(1);
-
 
 %% add video selection 
-base_path = '/home/mmvg/yuandi/OTB100'; %lx
-%ask the user for the video
+base_path = './data'; 
 video_path = choose_video(base_path);
 video=video_path(length(base_path)+1:end-1);
-% video = 'Skiing';
-
 [config]=config_list(video,base_path);
-
-display = 1;
-
-  
+display = 1; 
 result=tracking_base(opts,varargin,config,display);
-% result=drt_base(opts,varargin,config,display);   % drt with calculate the scale and position simultaneously
-%%
-[distance_precision, area, average_center_location_error] = ...
-    compute_performance_measures(result, config.gt);
-
-fprintf('\n%f\n',area);
-  
-       
-
-
-
